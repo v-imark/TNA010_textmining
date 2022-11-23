aa4572e2f7f71ee4810b9934da58ebfbc4766bce
gabce093 <gc.yppoo@gmail.com>
2022-11-23 15:39:54 +0100

    Quick fix of previous push

--- getHk.m
+++ getHk.m
@@ -1,8 +1,12 @@
-function Hk = getHk(S, Vt, k)
+function Hk = getHk(S, V, k)
 %getHk Return Hk = Sigma_k * V_T_k
 Sk = S(1:k, 1:k);
-Vtk = Vt(1:k, :);
-
+Vtk = V(:, 1:k)';
 Hk = Sk*Vtk;
+
+% H = S*Vt;
+% Hk = H(1:k, :);
+
+
 end
 
